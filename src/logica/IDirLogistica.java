615b9d085d13c37330923be07b4738dc8749fd37
Juan Moreno <jupmorenor@gmail.com>
2015-11-25 07:23:48 -0500

    modificar seccion en ILogistica

--- src/logica/IDirLogistica.java
+++ src/logica/IDirLogistica.java
@@ -7,5 +7,7 @@ public interface IDirLogistica {
 	public abstract void crearEvento(Date f, String nom, int ed, int val);
 
 	public abstract void modificarEvento(String nom, Date f);
+	
+	public abstract void modificarSeccion();
 
 }
