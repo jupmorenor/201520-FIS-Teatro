4f856c05903390f1aa1375bf3bcd22891a5f1d84
Alejandro <Alejandro@Alejandro-PC>
2015-11-25 09:19:49 -0500

    Datos de seguridad

--- src/GUI/Logistica/LAcomodar.java
+++ src/GUI/Logistica/LAcomodar.java
@@ -2,6 +2,7 @@ package GUI.Logistica;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Graphics;
 import java.util.Date;
 
 import javax.swing.BorderFactory;
@@ -19,13 +20,19 @@ import javax.swing.border.Border;
  */
 public class LAcomodar extends JFrame{
     
-    private final JLabel[][] palco1;
-    private final JLabel[][] palco2;
-    private final JLabel[][] palco3;
-    private final JLabel[][] palco4;
-    private final JLabel[][] platea;
-    private final JLabel[][] anfiteatro;
-
+    private final JLabel[][] mtzPalco1;
+    private final JLabel[][] mtzPalco2;
+    private final JLabel[][] mtzPalco3;
+    private final JLabel[][] mtzPalco4;
+    private final JLabel[][] mtzPlatea;
+    private final JLabel[][] mtzAnfiteatro;
+    
+    private final JTextField txtPalco1;
+    private final JTextField txtPalco2;
+    private final JTextField txtPalco3;
+    private final JTextField txtPalco4;
+    private final JTextField txtPlatea;
+    private final JTextField txtAnfiteatro;
     
     public LAcomodar(Logistica logistica) {
     	
@@ -36,97 +43,115 @@ public class LAcomodar extends JFrame{
     	Container contenedor = getContentPane();
         contenedor.setLayout(null);
         
-        JLabel lblPalco1 = new JLabel();
-        lblPalco1.setBounds(30, 30, 50, 23);
-        lblPalco1.setText("Palco1");
-        contenedor.add(lblPalco1);
+        JLabel lblMtzPalco1 = new JLabel();
+        lblMtzPalco1.setBounds(20, 30, 50, 23);
+        lblMtzPalco1.setText("Palco1");
+        contenedor.add(lblMtzPalco1);
         
-        palco1 = new JLabel[2][3];
-    	for(int i=0; i<2; i++){
-    		for(int j=0; j<3; j++){
-    			palco1[i][j] = new JLabel();
-    			palco1[i][j].setBounds(30*i+20, 30*j+55, 30, 30);
-    			palco1[i][j].setBorder(borde);
-    			contenedor.add(palco1[i][j]);
+        mtzPalco1 = new JLabel[3][5];
+    	for(int i=0; i<3; i++){
+    		for(int j=0; j<5; j++){
+    			mtzPalco1[i][j] = new JLabel();
+    			mtzPalco1[i][j].setBounds(30*i+20, 30*j+50, 30, 30);
+    			mtzPalco1[i][j].setBorder(borde);
+    			mtzPalco1[i][j].setName("1");
+    			contenedor.add(mtzPalco1[i][j]);
     		}
     	}
     	
-    	JLabel lblPalco2 = new JLabel();
-        lblPalco2.setBounds(30, 190, 50, 23);
-        lblPalco2.setText("Palco2");
-        contenedor.add(lblPalco2);
+    	JLabel lblMtzPalco2 = new JLabel();
+        lblMtzPalco2.setBounds(20, 310, 50, 23);
+        lblMtzPalco2.setText("Palco2");
+        contenedor.add(lblMtzPalco2);
     	
-    	palco2 = new JLabel[2][3];
-    	for(int i=0; i<2; i++){
-    		for(int j=0; j<3; j++){
-    			palco2[i][j] = new JLabel();
-    			palco2[i][j].setBounds(30*i+20, 30*j+215, 30, 30);
-    			palco2[i][j].setBorder(borde);
-    			contenedor.add(palco2[i][j]);
+    	mtzPalco2 = new JLabel[3][5];
+    	for(int i=0; i<3; i++){
+    		for(int j=0; j<5; j++){
+    			mtzPalco2[i][j] = new JLabel();
+    			mtzPalco2[i][j].setBounds(30*i+20, 30*j+330, 30, 30);
+    			mtzPalco2[i][j].setBorder(borde);
+    			mtzPalco2[i][j].setName("1");
+    			contenedor.add(mtzPalco2[i][j]);
     		}
     	}
     	
-    	JLabel lblPalco3 = new JLabel();
-        lblPalco3.setBounds(430, 30, 50, 23);
-        lblPalco3.setText("Palco3");
-        contenedor.add(lblPalco3);
+    	JLabel lblMtzPalco3 = new JLabel();
+        lblMtzPalco3.setBounds(650, 30, 50, 23);
+        lblMtzPalco3.setText("Palco3");
+        contenedor.add(lblMtzPalco3);
     	
-    	palco3 = new JLabel[2][3];
-    	for(int i=0; i<2; i++){
-    		for(int j=0; j<3; j++){
-    			palco3[i][j] = new JLabel();
-    			palco3[i][j].setBounds(30*i+420, 30*j+55, 30, 30);
-    			palco3[i][j].setBorder(borde);
-    			contenedor.add(palco3[i][j]);
+    	mtzPalco3 = new JLabel[3][5];
+    	for(int i=0; i<3; i++){
+    		for(int j=0; j<5; j++){
+    			mtzPalco3[i][j] = new JLabel();
+    			mtzPalco3[i][j].setBounds(30*i+650, 30*j+50, 30, 30);
+    			mtzPalco3[i][j].setBorder(borde);
+    			mtzPalco3[i][j].setName("1");
+    			contenedor.add(mtzPalco3[i][j]);
     		}
     	}
     	
-    	JLabel lblPalco4 = new JLabel();
-        lblPalco4.setBounds(430, 190, 50, 23);
-        lblPalco4.setText("Palco4");
-        contenedor.add(lblPalco4);
+    	JLabel lblMtzPalco4 = new JLabel();
+        lblMtzPalco4.setBounds(650, 310, 50, 23);
+        lblMtzPalco4.setText("Palco4");
+        contenedor.add(lblMtzPalco4);
     	
-    	palco4 = new JLabel[2][3];
-    	for(int i=0; i<2; i++){
-    		for(int j=0; j<3; j++){
-    			palco4[i][j] = new JLabel();
-    			palco4[i][j].setBounds(30*i+420, 30*j+215, 30, 30);
-    			palco4[i][j].setBorder(borde);
-    			contenedor.add(palco4[i][j]);
-    		}
-    	}
-    	
-    	anfiteatro = new JLabel[10][4];
-    	for(int i=0; i<10; i++){
-    		for(int j=0; j<4; j++){
-    			anfiteatro[i][j] = new JLabel();
-    			anfiteatro[i][j].setBounds(30*i+100, 30*j+40, 30, 30);
-    			anfiteatro[i][j].setBorder(borde);
-    			contenedor.add(anfiteatro[i][j]);
-    		}
-    	}
-    	
-    	platea = new JLabel[10][4];
-    	for(int i=0; i<10; i++){
-    		for(int j=0; j<4; j++){
-    			platea[i][j] = new JLabel();
-    			platea[i][j].setBounds(30*i+100, 30*j+200, 30, 30);
-    			platea[i][j].setBorder(borde);
-    			contenedor.add(platea[i][j]);
+    	mtzPalco4 = new JLabel[3][5];
+    	for(int i=0; i<3; i++){
+    		for(int j=0; j<5; j++){
+    			mtzPalco4[i][j] = new JLabel();
+    			mtzPalco4[i][j].setBounds(30*i+650, 30*j+330, 30, 30);
+    			mtzPalco4[i][j].setBorder(borde);
+    			mtzPalco4[i][j].setName("1");
+    			contenedor.add(mtzPalco4[i][j]);
     		}
     	}
     	
     	
+    	JLabel lblMtzAnfiteatro = new JLabel();
+        lblMtzAnfiteatro.setBounds(150, 30, 80, 23);
+        lblMtzAnfiteatro.setText("Anfiteatro");
+        contenedor.add(lblMtzAnfiteatro);
         
-
+    	mtzAnfiteatro = new JLabel[15][8];
+    	for(int i=0; i<15; i++){
+    		for(int j=0; j<8; j++){
+    			mtzAnfiteatro[i][j] = new JLabel();
+    			mtzAnfiteatro[i][j].setBounds(30*i+150, 30*j+50, 30, 30);
+    			mtzAnfiteatro[i][j].setBorder(borde);
+    			mtzAnfiteatro[i][j].setName("1");
+    			contenedor.add(mtzAnfiteatro[i][j]);
+    		}
+    	}
+    	
+    	
+    	JLabel lblMtzPlatea = new JLabel();
+        lblMtzPlatea.setBounds(150, 310, 80, 23);
+        lblMtzPlatea.setText("Platea");
+        contenedor.add(lblMtzPlatea);
         
-        
+    	mtzPlatea = new JLabel[15][5];
+    	for(int i=0; i<15; i++){
+    		for(int j=0; j<5; j++){
+    			mtzPlatea[i][j] = new JLabel();
+    			mtzPlatea[i][j].setBounds(30*i+150, 30*j+330, 30, 30);
+    			mtzPlatea[i][j].setBorder(borde);
+    			mtzPlatea[i][j].setName("1");
+    			contenedor.add(mtzPlatea[i][j]);
+    		}
+    	}
         
         this.pack();
-        this.setBounds(300, 200, 600, 400);
+        this.setBounds(100, 100, 1200, 550);
         this.setVisible(true);
+        this.repaint();
     }
 
+    public void paint(Graphics g){
+    	super.paint(g);
+    	g.drawLine(800, 0, 800, 550);
+    }
+    
 	private void btnCrearActionPerformed() {
 		
 		
